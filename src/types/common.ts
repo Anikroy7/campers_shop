354be96830408f8ab026fd209197cc99111bb53b
Anikroy7 <roy645800@gmail.com>
2024-07-29 20:30:35 +0600

    types added

---
+++ src/types/common.ts
@@ -0,0 +1,35 @@
+export type TProduct = {
+    name: string;
+    price: number;
+    stockQuantity: number;
+    description: string;
+    category: string;
+    ratings: number;
+    images?: string[]
+    isDeleted: boolean;
+    quantity: number
+}
+
+export type TOrdrededProduct = {
+    productId: string;
+    quantity: number
+}
+
+export type TOrderedUserInfo={
+    name: string;
+    email: string;
+    address: string;
+    contactNo: string;
+}
+
+export type TOrder = {
+    name: string;
+    email: string;
+    address: string;
+    contactNo: string;
+    totalPrice: number;
+    products: TOrdrededProduct[];
+    clientSecret: string;
+    paymentIntent: string;
+    paymentMethod: string;
+};
